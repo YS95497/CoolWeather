ca83c1de867978a2e0ab97f81b74d5c8463f6b1a
1299296501@qq.com <1299296501@qq.com>
2020-03-27 08:14:38 +0000

    Add fragment.

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,25 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceNmae;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public String getProvinceNmae(){
+        return provinceNmae;
+    }
+    public void setProvinceNmae(String provinceNmae){
+        this.provinceNmae = provinceNmae;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
